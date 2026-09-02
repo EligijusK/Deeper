f5c0ec6b0926d000589e278c4b81eab3a9d81aeb
EligijusK <e.kiudys@gmail.com>
2026-09-02 03:28:25 +0300

    Preparing for login functionality

---
+++ shared/src/commonMain/kotlin/com/eligijus/deeper/presentation/login/LoginViewModel.kt
@@ -0,0 +1,67 @@
+package com.eligijus.deeper.presentation.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.eligijus.deeper.domain.usecase.LoginUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class LoginViewModel (
+     private val loginUseCase: LoginUseCase
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(LoginUiState())
+
+    val uiState: StateFlow<LoginUiState> =
+        _uiState.asStateFlow()
+
+    fun onEmailChanged(email: String) {
+        _uiState.update {
+            it.copy(
+                email = email,
+                errorMessage = null
+            )
+        }
+    }
+
+    fun onPasswordChanged(password: String) {
+        _uiState.update {
+            it.copy(
+                password = password,
+                errorMessage = null
+            )
+        }
+    }
+
+    fun login() {
+        viewModelScope.launch {
+            _uiState.update {
+                it.copy(
+                    isLoading = true,
+                    errorMessage = null
+                )
+            }
+
+            val result = loginUseCase(
+                email = uiState.value.email,
+                password = uiState.value.password
+            )
+
+            result
+                .onSuccess {
+                    // Later: navigate / expose successful login event
+                }
+                .onFailure { error ->
+                    _uiState.update {
+                        it.copy(
+                            errorMessage = error.message,
+                            isLoading = false
+                        )
+                    }
+                }
+        }
+    }
+}
